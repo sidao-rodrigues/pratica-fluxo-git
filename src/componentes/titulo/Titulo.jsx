d02ed7a8579e8e9f71c145d931187d6279871b89
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2025-12-06 11:06:27 -0300

    feat: atualizacao dos textos do componente de titulo

--- src/componentes/titulo/Titulo.jsx
+++ src/componentes/titulo/Titulo.jsx
@@ -3,8 +3,8 @@ import './Titulo.css';
 function Titulo() {
   return (
     <header className="titulo-container">
-      <h1 className="titulo-principal">Prática de Git - IFCE</h1>
-      <p className="titulo-subtitulo">Aprendendo fluxo de trabalho com Git</p>
+      <h1 className="titulo-principal">Prática de ADS</h1>
+      <p className="titulo-subtitulo">Aprendendo fluxo de trabalho com Git na aula de sábado em Dezembro LoL</p>
     </header>
   );
 }
